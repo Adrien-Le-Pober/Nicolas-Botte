d9bc0659df9d11f4d6719fddd9d6ecc55955cb9a
Zenithao <adrien.le.pober@gmail.com>
2022-05-29 16:59:31 +0200

    add tarifs crud

---
+++ src/Form/TarifType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tarif;
+use App\Entity\Service;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TarifType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('price')
+            ->add('service', EntityType::class, [
+                // looks for choices from this entity
+                'class' => Service::class,
+            
+                // uses the User.username property as the visible option string
+                'choice_label' => 'name',
+            
+                // used to render a select box, check boxes or radios
+                // 'multiple' => true,
+                // 'expanded' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tarif::class,
+        ]);
+    }
+}

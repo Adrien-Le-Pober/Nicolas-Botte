0ef279dd329e4027d5196c3a71d07221489a2a9a
Zenithao <adrien.le.pober@gmail.com>
2022-06-28 21:48:11 +0200

    add contact form

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(
+            Request $request,
+            MailerInterface $mailer
+        ): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $lastname = $data['lastname'];
+            $firstname = $data['firstname'];
+            $email = (new TemplatedEmail())
+                ->from('hello@example.com')
+                ->to('you@example.com')
+                ->subject("Nouveau message de $lastname $firstname")
+                ->htmlTemplate('emails/contact.html.twig')
+                ->context([
+                    'data' => $data
+                ]);
+
+            $mailer->send($email);
+
+            $session = $request->getSession();
+            $session->getFlashBag()->add('success', 'Merci, votre message a bien été envoyé');
+
+            return $this->redirectToRoute('app_contact', ['_fragment' => 'contact'], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Form/ContactType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastname', TextType::class, [
+                'required' => true,
+                'constraints' => [new Length([
+                    'min' => 2,
+                    'max' => 100
+                    ])],
+            ])
+            ->add('firstname', TextType::class, [
+                'required' => true,
+                'constraints' => [new Length([
+                    'min' => 2,
+                    'max' => 100
+                    ])],
+            ])
+            ->add('phone', TextType::class, [
+                'required' => false,
+                'constraints' => [new Length([
+                    'min' => 4,
+                    'max' => 20
+                    ])],
+            ])
+            ->add('email', EmailType::class, [
+                'required' => false,
+                'constraints' => [new Length([
+                    'min' => 3,
+                    'max' => 1000
+                    ])],
+            ])
+            ->add('message', TextType::class, [
+                'required' => true,
+                'constraints' => [new Length([
+                    'min' => 3,
+                    'max' => 10000
+                    ])],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}

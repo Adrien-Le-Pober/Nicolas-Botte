5aba67e783c59b7e7ae1cf9c590f1c98fc4e3821
Zenithao <adrien.le.pober@gmail.com>
2022-08-30 17:21:08 +0200

    fix migration cascade on atelier and tarif

--- migrations/Version20220830151939.php
+++ migrations/Version20220830151939.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220830144025 extends AbstractMigration
+final class Version20220830151939 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,19 +21,15 @@ final class Version20220830144025 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE atelier DROP FOREIGN KEY FK_E1BB1823ED5CA9E6');
-        $this->addSql('ALTER TABLE atelier CHANGE service_id service_id INT NOT NULL');
+        $this->addSql('ALTER TABLE atelier CHANGE service_id service_id INT NOT NULL, CHANGE short_description short_description VARCHAR(160) DEFAULT NULL');
         $this->addSql('ALTER TABLE atelier ADD CONSTRAINT FK_E1BB1823ED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE tarif DROP FOREIGN KEY FK_E7189C9ED5CA9E6');
-        $this->addSql('ALTER TABLE tarif ADD CONSTRAINT FK_E7189C9ED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE atelier DROP FOREIGN KEY FK_E1BB1823ED5CA9E6');
-        $this->addSql('ALTER TABLE atelier CHANGE service_id service_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE atelier CHANGE service_id service_id INT DEFAULT NULL, CHANGE short_description short_description VARCHAR(500) DEFAULT NULL');
         $this->addSql('ALTER TABLE atelier ADD CONSTRAINT FK_E1BB1823ED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('ALTER TABLE tarif DROP FOREIGN KEY FK_E7189C9ED5CA9E6');
-        $this->addSql('ALTER TABLE tarif ADD CONSTRAINT FK_E7189C9ED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
     }
 }
